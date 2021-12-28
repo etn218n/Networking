eb5ef92200429a99bea58953419a9a5c37e9d1a0
Duy <etn218n@gmail.com>
2021-12-28 19:24:05 +0200

    Implement player connection and spawning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using Mirror;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,48 @@
+﻿using Mirror;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : NetworkBehaviour
+{
+    [Header("Visual")]
+    [SerializeField] private Renderer mainRenderer;
+    [SerializeField] private List<Material> colorMaterials;
+    
+    [Header("Spawn Settings")] 
+    [Range(1f, 20f)]
+    [SerializeField] private float spawnRadius;
+
+    [SyncVar(hook = nameof(OnColorIndexUpdated))] 
+    private int currentColorIndex;
+
+    public override void OnStartLocalPlayer()
+    {
+        CmdRequestColor();
+        CmdRequestSpawnPosition();
+    }
+
+    [Command]
+    private void CmdRequestSpawnPosition()
+    {
+        var randomPoint = Random.insideUnitCircle * spawnRadius;
+
+        transform.position = new Vector3(randomPoint.x, transform.position.y, randomPoint.y);
+    }
+
+    [Command]
+    private void CmdRequestColor()
+    {
+        if (colorMaterials.Count == 0)
+            return;
+        
+        var index = (NetworkManager.singleton.numPlayers - 1) % colorMaterials.Count;
+
+        currentColorIndex = index;
+        mainRenderer.material = colorMaterials[index];
+    }
+
+    private void OnColorIndexUpdated(int oldIndex, int newIndex)
+    {
+        mainRenderer.material = colorMaterials[newIndex];
+    }
+}
