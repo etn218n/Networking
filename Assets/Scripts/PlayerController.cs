8551c760b614760b14e1a69cf1641e85a976d274
Duy <etn218n@gmail.com>
2021-12-28 20:00:44 +0200

    Test server authoritative movement without client prediction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,48 @@
 using Mirror;
+using UnityEngine;
 
 public class PlayerController : NetworkBehaviour
 {
+    [SerializeField] private float moveSpeed;
+
+    private Vector3 desiredVelocity;
+    private Rigidbody rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (hasAuthority)
+            ProcessInput();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isServer)
+            Move();
+    }
+
+    [Server]
+    private void Move()
+    {
+        rigidBody.MovePosition(rigidBody.position + desiredVelocity * Time.fixedDeltaTime);
+    }
+
+    [Client]
+    private void ProcessInput()
+    {
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+
+        CmdSendInputToServer(new Vector3(x, 0f, y));
+    }
+
+    [Command]
+    private void CmdSendInputToServer(Vector3 inputVector)
+    {
+        desiredVelocity = inputVector.normalized * moveSpeed;
+    }
 }
